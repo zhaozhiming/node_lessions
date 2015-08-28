731c0b674259d0864f05329d29fd46e3bfd714ed
zhaozhiming <kingzzm1982@sina.com>
2015-08-28 14:40:06 +0800

    lesson6

---
+++ lesson6/main.js
@@ -0,0 +1,15 @@
+var fibonacci = function (n) {
+  if (n === 0 || n === 1) return n;
+  if (n > 10) throw new Error('n should <= 10');
+  if (n < 0) throw new Error('n should >= 0');
+  if (typeof n !== 'number') throw new Error('n should be a number');
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+if (require.main === module) {
+  var n = Number(process.argv[2]);
+  console.log('fibonacci(' + n + ') is ', fibonacci(n));
+}
+
+exports.fibonacci = fibonacci;
